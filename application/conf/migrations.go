ad1420a2ec6e48a02595a71685245d6f34895f3d
Jacques Maarek <jacques.maarek@hetic.net>
2020-10-08 11:19:19 +0200

    Feat: Added order model in migrations.

--- application/conf/migrations.go
+++ application/conf/migrations.go
@@ -4,5 +4,5 @@ import "github.com/JackMaarek/cqrsPattern/application/models"
 
 // MakeMigrations executes all migrations for our structs
 func MakeMigrations() {
-	DB.AutoMigrate(&models.User{})
+	DB.AutoMigrate(&models.User{}, &models.Order{})
 }
