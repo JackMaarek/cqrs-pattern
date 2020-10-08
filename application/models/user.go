4391f4ae40b81557aaabf2c7985e62d69265b49d
Jacques Maarek <jacques.maarek@hetic.net>
2020-10-08 11:17:50 +0200

    Fix: formated user model file.

--- application/models/user.go
+++ application/models/user.go
@@ -1,11 +1,11 @@
 package models
 
 type User struct {
-	ID       uint64 `gorm:"primary_key" json:"id"`
-	Name     string `gorm:"size:255" json:"name"`
-	Surname  string `gorm:"size:255" json:"surname"`
-	Password string `gorm:"size:255" json:"-"`
-	Email    string `gorm:"size:255" json:"email"`
+	ID       uint64  `gorm:"primary_key" json:"id"`
+	Name     string  `gorm:"size:255" json:"name"`
+	Surname  string  `gorm:"size:255" json:"surname"`
+	Password string  `gorm:"size:255" json:"-"`
+	Email    string  `gorm:"size:255" json:"email"`
 }
 
 type UserAccount struct {
